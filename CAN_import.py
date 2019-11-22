b2b1a9d2765fe3e9691f82f5bb4eebdf6eaac79b
nightlamp <benjaminlongbottom98@gmail.com>
2019-11-22 21:01:31 +0800

    got it working with good predictions overall

--- CAN_import.py
+++ CAN_import.py
@@ -2,6 +2,33 @@
 
 import numpy as np
 
-def extract_arr_from_csv(filename):
+
+
+# reads csv from file and returns an array
+def read_csv(filename):
     return np.genfromtxt(filename, delimiter=',', dtype=None, encoding='utf8')
 
+
+# get the data and target 
+def simple_extract_data(arr):
+    arr_len = len(arr)
+    data   = []
+    target = []
+    # iterate throgh each data point and extract relevent data and target
+    for i in range(arr_len):
+        # extract data from raw data
+        can_id = int(arr[i][1], 16)
+        data_point = [arr[i][0], can_id]
+        # append datapoint and corresponding target to lists
+        data.append(data_point) 
+        target.append(arr[i][-1])
+
+    return data, target
+
+
+
+# gets the data and target from csv file
+def data_from_csv(filename):
+    arr = read_csv(filename)
+    data, target = simple_extract_data(arr)
+    return data, target
--- simple_ids.py
+++ simple_ids.py
@@ -4,35 +4,70 @@ import sys
 import numpy as np
 import sklearn as skl
 from sklearn import svm
-from CAN_import import extract_arr_from_csv
+from CAN_import import data_from_csv
 
 
 
-##### test func
+##### helper funcs
+def get_accuracy(target, pred, splitpoint):
+    pred_len = len(pred)
+    accuracy = 0
+    # count how many were correct
+    for i in range(pred_len):
+        if target[i + splitpoint] == pred[i]:
+            accuracy += 1
+    return accuracy/pred_len
+
+
+def clfs_from_files(files):
+    # vars
+    clfs  = []
+    preds = []
+
+    # make classifiers from csv files
+    for i in range(len(files)):
+        # init clasifier for file
+        clf = svm.SVC(gamma=0.001, C=100.)
+        
+        # get data from relevent file
+        data, target = data_from_csv(files[i])
+        data_len = len(data)
+
+        # get split index 
+        splitpoint = data_len - 2000
+
+        # fit data up to splitpoint, leaving the rest to check accuracy
+        clf.fit(data[:splitpoint], target[:splitpoint])
+        print('data fitting for file[{}] complete'.format(i))
+
+        # check accuracy of output
+        pred = clf.predict(data[splitpoint:])
+        accuracy = get_accuracy(target, pred, splitpoint)
+        print('accuracy is {}'.format(accuracy))
+        
+        # store clf and pred
+        preds.append(pred)
+        clfs.append(clf)
+    return clfs, preds
+
+
+    
+
+##### main func
 def main():
-    data_dir     = '../datasets/trainingData/'
-    file_normal  = data_dir + 'HYUNDAI_SONATA_Attack_free_TRAIN_Release.csv'
-    file_flood   = data_dir + 'HYUNDAI_SONATA_Flooding_TRAIN_Release.csv'
-    file_fuzzy   = data_dir + 'HYUNDAI_SONATA_Fuzzy_TRAIN_Release.csv'
-    file_malfunc = data_dir + 'HYUNDAI_SONATA_Malfunction_TRAIN_Release.csv'
+    # set file names
+    data_dir = '../datasets/trainingData/'
+    car_name = 'HYUNDAI_SONATA'
+    file_nml = data_dir + car_name + '_Attack_free_TRAIN_Release.csv'
+    file_fld = data_dir + car_name + '_Flooding_TRAIN_Release.csv'
+    file_fuz = data_dir + car_name + '_Fuzzy_TRAIN_Release.csv'
+    file_mal = data_dir + car_name + '_Malfunction_TRAIN_Release.csv'
+    files = [file_fld, file_fuz, file_mal]
+    
+    # make classifiers and get predictions for all files
+    clfs, preds = clfs_from_files(files)
 
-    arr     = extract_arr_from_csv(file_flood)
-    arr_len = len(arr)
-
-    # split data
-    data   = [ [arr[i][0], int(arr[i][1], 16)] for i in range(arr_len) ]
-    target = [ arr[i][-1] for i in range(arr_len) ]
-
-    clf = svm.SVC(gamma=0.001, C=100.)
-
-    splitpoint = arr_len - 10
-
-    clf.fit(data[:splitpoint], target[:splitpoint])
-    print('data fitting complete')
-
-    pred = clf.predict(data[splitpoint:])
-    print("prediction of last msg = {}".format(pred))
-    return clf
+    return clfs, preds
 
 
 
