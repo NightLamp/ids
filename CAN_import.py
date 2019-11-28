6f7806a349c95f0a580bd45781ae0665ef8c4498
nightlamp <benjaminlongbottom98@gmail.com>
2019-11-28 18:28:04 +0800

    got loading and storing ids working

--- CAN_import.py
+++ CAN_import.py
@@ -1,6 +1,7 @@
 # intrusion detection system using machine learning
 
 import numpy as np
+import csv
 
 
 
@@ -8,6 +9,13 @@ import numpy as np
 def read_csv(filename):
     return np.genfromtxt(filename, delimiter=',', dtype=None, encoding='utf8')
 
+def write_data_to_csv(filename, data):
+    with open(filename, 'w') as csvfile:
+        csv.writer(filename)
+        for msgno in range(len(data)):
+            csv.writerow([msgno+1, msg[msgno]])
+    return
+    
 
 # get the data and target 
 def simple_extract_data(arr):
@@ -15,10 +23,11 @@ def simple_extract_data(arr):
     data   = []
     target = []
     # iterate throgh each data point and extract relevent data and target
+    start_time = arr[0][0]
     for i in range(arr_len):
         # extract data from raw data
         can_id = int(arr[i][1], 16)
-        can_timestamp = arr[i][0] - arr[0][0]
+        can_timestamp = arr[i][0] - start_time
         data_point = [can_timestamp, can_id]
         # append datapoint and corresponding target to lists
         data.append(data_point) 
@@ -28,8 +37,33 @@ def simple_extract_data(arr):
 
 
 
+# get the data and target 
+def simple_extract_test_data(arr):
+    arr_len = len(arr)
+    data   = []
+    start_time = arr[0][1]
+    # iterate throgh each data point and extract relevent data and target
+    for i in range(arr_len):
+        # extract data from raw data
+        can_id = int(arr[i][2], 16)
+        can_timestamp = arr[i][1] - start_time
+        data_point = [can_timestamp, can_id]
+        # append datapoint and corresponding target to lists
+        data.append(data_point) 
+
+    return data
+
+
+
+
 # gets the data and target from csv file
 def data_from_csv(filename):
     arr = read_csv(filename)
     data, target = simple_extract_data(arr)
     return data, target
+
+
+def data_from_test_csv(filename):
+    arr = read_csv(filename)
+    data = simple_extract_test_data(arr)
+    return data
--- simple_ids.py
+++ simple_ids.py
@@ -4,7 +4,9 @@ import sys
 import numpy as np
 import sklearn as skl
 from sklearn import svm
-from CAN_import import data_from_csv
+from CAN_import import data_from_csv, data_from_test_csv
+import joblib
+import os
 
 
 # globals
@@ -19,20 +21,23 @@ class ids:
 
     # init ids class and teach the machine learning algorithms
     def __init__(self, msg_files):
-        for msg_file in msg_files:
-            # init clasifier for file
-            clf = svm.SVC(gamma=0.001, C=100.)
-            
-            # get data from relevent file
-            msgs, msg_type = data_from_csv(msg_file)
-            msgs_len = len(msgs)
+        if msg_files != None:
+            for msg_file in msg_files:
+                # init clasifier for file
+                clf = svm.SVC(gamma=0.001, C=100.)
+                
+                # get data from relevent file
+                msgs, msg_type = data_from_csv(msg_file)
+                msgs_len = len(msgs)
 
-            # get split index 
-            splitpoint = msgs_len - 2000
+                # get split index 
+                splitpoint = msgs_len - 2000
 
-            # fit msgs up to splitpoint, leaving the rest to check accuracy
-            clf.fit(msgs[:splitpoint], msg_type[:splitpoint])
-            self.clfs.append(clf)
+                # fit msgs up to splitpoint, leaving the rest to check accuracy
+                clf.fit(msgs[:splitpoint], msg_type[:splitpoint])
+                self.clfs.append(clf)
+                print('a classifier has been appended')
+        return
 
 
     # predict the type of message with Machine Learning.
@@ -49,7 +54,23 @@ class ids:
                     msg_types[i] = attack
 
         return msg_types 
+    
+    
+    def save_ids(self, path):
+        for i in range(len(self.clfs)):
+            clf = self.clfs[i]
+            clf_path = path + '/clf_' + str(i) + '.clf'
+            joblib.dump(clf_path, clf)
+        return
+
+    def load_ids(self, path):
+        for clf_path in os.listdir(path):
+            clf_path = path + '/' + clf_path
+            clf = joblib.load(clf_path)
+            self.clfs.append(clf)
+        return 
         
+            
 
 
 
@@ -66,50 +87,89 @@ def get_accuracy(target, pred):
     return accuracy/pred_len
 
 
-def clfs_from_files(files):
-    # vars
-    clfs  = []
-    preds = []
+#def clfs_from_files(files):
+#    # vars
+#    clfs  = []
+#    preds = []
+#
+#    # make classifiers from csv files
+#    for i in range(len(files)):
+#        # init clasifier for file
+#        clf = svm.SVC(gamma=0.001, C=100.)
+#        
+#        # get data from relevent file
+#        data, target = data_from_csv(files[i])
+#        data_len = len(data)
+#
+#        # get split index 
+#        splitpoint = data_len - 2000
+#
+#        # fit data up to splitpoint, leaving the rest to check accuracy
+#        clf.fit(data[:splitpoint], target[:splitpoint])
+#        print('data fitting for file[{}] complete'.format(i))
+#
+#        # check accuracy of output
+#        pred = clf.predict(data[splitpoint:])
+#        accuracy = get_accuracy(target[splitpoint:], pred)
+#        print('accuracy is {}'.format(accuracy))
+#        
+#        # store clf and pred
+#        preds.append(pred)
+#        clfs.append(clf)
+#    return clfs, preds
 
-    # make classifiers from csv files
-    for i in range(len(files)):
-        # init clasifier for file
-        clf = svm.SVC(gamma=0.001, C=100.)
-        
-        # get data from relevent file
-        data, target = data_from_csv(files[i])
-        data_len = len(data)
 
-        # get split index 
-        splitpoint = data_len - 2000
-
-        # fit data up to splitpoint, leaving the rest to check accuracy
-        clf.fit(data[:splitpoint], target[:splitpoint])
-        print('data fitting for file[{}] complete'.format(i))
-
-        # check accuracy of output
-        pred = clf.predict(data[splitpoint:])
-        accuracy = get_accuracy(target[splitpoint:], pred)
-        print('accuracy is {}'.format(accuracy))
-        
-        # store clf and pred
-        preds.append(pred)
-        clfs.append(clf)
-    return clfs, preds
-
-
+def make_ids():
+    data_dir = '../datasets/trainingData/'
+    car_names = ['HYUNDAI_SONATA', 'KIA_SOUL', 'CHEVORLET_SPARK']
+    files = []
+    for car_name in car_names:
+        file_nml = data_dir + car_name + '_Attack_free_TRAIN_Release.csv'
+        file_fld = data_dir + car_name + '_Flooding_TRAIN_Release.csv'
+        file_fuz = data_dir + car_name + '_Fuzzy_TRAIN_Release.csv'
+        file_mal = data_dir + car_name + '_Malfunction_TRAIN_Release.csv'
+        car_files = [file_fld, file_fuz, file_mal]
+        files += car_files
     
+    # make ids    
+    myIDS = ids(files)
+    return myIDS
+
+
+def predict_test_files(ids):
+    data_dir = '../datasets/TestingData/'
+    big_pred_list = []
+    for fileno in [1,2,3,4,7,8,9]:
+        filename = data_dir + 'File_{}.csv'.format(fileno)
+        data = data_from_test_csv(filename)
+        pred = ids.predict_msg_type(data)       
+        big_pred_list += pred
+ 
+    return big_pred_list
+        
+
+def save_prediction(filename, data):
+    with open(filename, data) as fp:
+        fp.write('Class,Number')
+        for i in range(len(data)):
+            fp.write(str(i+1) + ',' + data[i] + '\n')
+    return
+        
+     
 
 ##### main func
 def main():
     # set file names
     data_dir = '../datasets/trainingData/'
-    car_name = 'HYUNDAI_SONATA'
-    file_nml = data_dir + car_name + '_Attack_free_TRAIN_Release.csv'
-    file_fld = data_dir + car_name + '_Flooding_TRAIN_Release.csv'
-    file_fuz = data_dir + car_name + '_Fuzzy_TRAIN_Release.csv'
-    file_mal = data_dir + car_name + '_Malfunction_TRAIN_Release.csv'
-    files = [file_fld, file_fuz, file_mal]
+    car_names = ['HYUNDAI_SONATA', 'KIA_SOUL', 'CHEVORLET_SPARK']
+    files = []
+    for car_name in car_names:
+        file_nml = data_dir + car_name + '_Attack_free_TRAIN_Release.csv'
+        file_fld = data_dir + car_name + '_Flooding_TRAIN_Release.csv'
+        file_fuz = data_dir + car_name + '_Fuzzy_TRAIN_Release.csv'
+        file_mal = data_dir + car_name + '_Malfunction_TRAIN_Release.csv'
+        car_files = [file_fld, file_fuz, file_mal]
+        files += car_files
     
     # make classifiers and get predictions for all files
     myIDS = ids(files)
@@ -128,3 +188,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
